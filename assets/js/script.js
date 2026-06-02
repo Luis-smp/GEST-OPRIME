915a6540bc804c95820662037ad60c5797736176
Luis-smp <luisotaviocerqueirasampaio@gmail.com>
2026-06-02 10:58:19 -0300

    Menu e responsividade

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const navMenu = document.querySelector('.nav_menu')
+const btnToggle = document.querySelector('.menu_toggle')
+
+btnToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show')
+    
+});
+
+window.addEventListener('click', (e) =>{
+    if(!btnToggle.contains(e.target)){
+        navMenu.classList.remove('show')
+    }
+    navMenu.classList.toggle("active");
+    btnToggle.classList.toggle("active");
+})
+
+
